bf8337265f95d40bcd6a15d8b02a284c5769de1e
BuraYu <burak.yueksel@hotmail.com>
2025-05-08 16:15:22 +0200

    feat: add hamburger menu for navbar

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,27 +1,71 @@
+"use client";
+
+import { useState } from "react";
+
 const Navbar = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
   return (
     <div className="flex justify-center bg-gray-100">
       <div className="max-w-[1400px] w-full px-4">
-        <nav className="flex justify-between items-center py-4 ">
+        <nav className="flex justify-between items-center py-4">
           <div className="text-xl font-bold">Logo</div>
-          <ul className="flex space-x-6">
-            <li>
-              <a href="#" className="text-gray-500 hover:text-gray-900">
+          {/* Hamburger Menu */}
+          <button
+            className="block md:hidden text-gray-500 focus:outline-none"
+            onClick={() => setIsMenuOpen(!isMenuOpen)}
+          >
+            <svg
+              className="w-6 h-6"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h16M4 18h16"
+              ></path>
+            </svg>
+          </button>
+
+          {/* Links */}
+          <ul
+            className={`${
+              isMenuOpen ? "block" : "hidden"
+            } absolute top-16 left-0 w-full bg-gray-100 md:static md:flex md:space-x-6 md:w-auto`}
+          >
+            <li className="border-b md:border-none">
+              <a
+                href="#"
+                className="block py-2 px-4 text-gray-500 hover:text-gray-900"
+              >
                 Link1
               </a>
             </li>
-            <li>
-              <a href="#" className="text-gray-500 hover:text-gray-900">
+            <li className="border-b md:border-none">
+              <a
+                href="#"
+                className="block py-2 px-4 text-gray-500 hover:text-gray-900"
+              >
                 Link2
               </a>
             </li>
-            <li>
-              <a href="#" className="text-gray-500 hover:text-gray-900">
+            <li className="border-b md:border-none">
+              <a
+                href="#"
+                className="block py-2 px-4 text-gray-500 hover:text-gray-900"
+              >
                 Link3
               </a>
             </li>
-            <li>
-              <a href="#" className="text-gray-500 hover:text-gray-900">
+            <li className="border-b md:border-none">
+              <a
+                href="#"
+                className="block py-2 px-4 text-gray-500 hover:text-gray-900"
+              >
                 Link4
               </a>
             </li>

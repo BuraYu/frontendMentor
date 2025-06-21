1b05ed876c89f4201eb5136586cfabb3911b95de
BuraYu <burak.yueksel@hotmail.com>
2025-06-21 23:30:20 +0200

    add: custom colors in tailwind config

--- passwordgenerator/tailwind.config.js
+++ passwordgenerator/tailwind.config.js
@@ -5,6 +5,16 @@ module.exports = {
       fontFamily: {
         jetbrains: ["JetBrainsMono", "monospace"],
       },
+      colors: {
+        almostWhite: "#E6E5EA",
+        red: "#F64A4A",
+        orange: "#FB7C58",
+        yellow: "#F8CD65",
+        neonGreen: "#A4FFAF",
+        grey: "#817D92",
+        darkGrey: "#24232C",
+        veryDarkGrey: "#18171F",
+      },
     },
   },
   plugins: [],

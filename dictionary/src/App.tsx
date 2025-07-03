f15f8e21352caaf390c1c75e4f9fb03b538df0ff
Sercan Yüksel <search@sercans-mbp.speedport.ip>
2025-07-04 00:32:08 +0200

    feat: dictionairy: add api call to dictionairy api

--- dictionary/src/App.tsx
+++ dictionary/src/App.tsx
@@ -1,22 +1,31 @@
-import { useState, useMemo } from "react";
-import { ThemeProvider, CssBaseline, Container } from "@mui/material";
-import theme from "./theme/theme";
-import Navbar from "./components/Navbar";
-import Hero from "./components/Hero";
+import { useState, useMemo, useEffect } from 'react';
+import { ThemeProvider, CssBaseline, Container } from '@mui/material';
+import theme from './theme/theme';
+import Navbar from './components/Navbar';
+import SearchInput from './components/SearchInput';
 
 export default function App() {
   const [darkMode, setDarkMode] = useState(false);
-  const [fontChoice, setFontChoice] = useState<"sans" | "serif" | "mono">(
-    "sans"
+  const [fontChoice, setFontChoice] = useState<'sans' | 'serif' | 'mono'>(
+    'sans'
   );
+  const [data, setData] = useState();
 
+  useEffect(() => {
+    fetch('https://api.dictionaryapi.dev/api/v2/entries/en/keyboard')
+      .then((response) => response.json())
+      .then((result) => setData(result))
+      .catch((error) => console.error('Error fetching', error));
+  }, []);
+
+  console.log(data);
   const muiTheme = useMemo(
     () => theme(darkMode, fontChoice),
     [darkMode, fontChoice]
   );
 
   return (
-    <Container maxWidth="lg">
+    <Container maxWidth='lg'>
       <ThemeProvider theme={muiTheme}>
         <CssBaseline />
         <Navbar
@@ -25,7 +34,7 @@ export default function App() {
           fontChoice={fontChoice}
           setFontChoice={setFontChoice}
         />
-        <Hero />
+        <SearchInput darkMode={darkMode} />
       </ThemeProvider>
     </Container>
   );
